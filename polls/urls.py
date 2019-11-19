00f1cb904b2fd165f7da79c4341b30f5d50d5290
李宗熹 <zong.xi@medsoftsys.com>
2019-11-19 19:41:49 +0800

    part4

--- polls/urls.py
+++ polls/urls.py
@@ -6,8 +6,9 @@ from . import views
 # 参数name url的名称 作用体现在html上 相当于在html文件可以通过 'app名称:url名称' 索引出某个url
 app_name = 'polls'                                                          # 注意 这里必须注册一个命名空间
 urlpatterns = [
-    path('', views.index, name = 'index'),                                  # /polls
-    path('<int:question_id>/', views.detail, name = 'detail'),             # /polls/1
-    path('<int:question_id>/results', views.results, name = 'results'),    # /polls/1/results
-    path('<int:question_id>/vote', views.vote, name = 'vote')              # /polls/vote
+    path('', views.IndexView.as_view(), name = 'index'),                        # /polls
+    # DetailView 需要从URL获取主键值 
+    path('<int:pk>/', views.DetailView.as_view(), name = 'detail'),             # /polls/1
+    path('<int:pk>/results/', views.ResultsView.as_view(), name = 'results'),   # /polls/1/results
+    path('<int:question_id>/vote/', views.vote, name = 'vote')                  # /polls/vote
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,40 +1,44 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse,HttpResponseRedirect, Http404
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 
-def index(request):
-    lastest_question_list = Question.objects.order_by('-pub_date')[:5]      # 根据pub_date排序
-    context = {
-        "latest_question_list" : lastest_question_list
-    }
+# ListView 显示一个对象的列表
+class IndexView(generic.ListView):
+    template_name = "polls_template/index.html"
+    context_object_name  = "latest_question_list"
 
-    return render(request, 'polls_template/index.html', context)    # 请求对象 模板 传递给模板的数据
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
-    '''
-    or =>
-    template = loader.get_template("polls_template/index.html")
-    return HttpResponse(template.render(context, request))
-    '''
+# DetailView 显示特定类型对象的详细页面 根据主键进行数据库取值
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls_template/detail.html"
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)    # 后面可以跟上任意个数的关键字参数 使用的是filter方法
-    '''
-    or =>
-    try:
-        question = Question.objects.get(pk = question_id)
-    except:
-        raise Http404("question does not exist")
-    '''
-
-    return render(request, "polls_template/detail.html", {'question' : question})
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls_template/results.html"
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        # 从post方法获取'choice'字段的内容 类似json 具体看html
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : "no choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        # 重定向到投票结果界面
+        return HttpResponseRedirect(reverse('polls:results', args = (question.id, )))
